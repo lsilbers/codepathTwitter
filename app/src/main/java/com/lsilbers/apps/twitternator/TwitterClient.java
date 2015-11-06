a4fe572637ed5d4f75aad51c5e8ea6e9a4fdcfb9
Leo Silberstein <lsilberstein@groupon.com>
2015-11-05 21:57:42 -0800

    Update Client

--- app/src/main/java/com/lsilbers/apps/twitternator/TwitterClient.java
+++ app/src/main/java/com/lsilbers/apps/twitternator/TwitterClient.java
@@ -7,7 +7,7 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 import org.scribe.builder.api.Api;
-import org.scribe.builder.api.FlickrApi;
+import org.scribe.builder.api.TwitterApi;
 
 /*
  * 
@@ -21,14 +21,14 @@ import org.scribe.builder.api.FlickrApi;
  * NOTE: You may want to rename this object based on the service i.e TwitterClient or FlickrClient
  * 
  */
-public class RestClient extends OAuthBaseClient {
-	public static final Class<? extends Api> REST_API_CLASS = FlickrApi.class; // Change this
-	public static final String REST_URL = "http://api.flickr.com/services"; // Change this, base API URL
-	public static final String REST_CONSUMER_KEY = "SOME_KEY";       // Change this
-	public static final String REST_CONSUMER_SECRET = "SOME_SECRET"; // Change this
-	public static final String REST_CALLBACK_URL = "oauth://cprest"; // Change this (here and in manifest)
+public class TwitterClient extends OAuthBaseClient {
+	public static final Class<? extends Api> REST_API_CLASS = TwitterApi.class; // Change this
+	public static final String REST_URL = "https://api.twitter.com/1.1"; // Change this, base API URL
+	public static final String REST_CONSUMER_KEY = "aJ2tlNrIKwpqRuwD9xvBd29iN";       // Change this
+	public static final String REST_CONSUMER_SECRET = "nLTxNOqZ7O7Nqql3nqC4pkzGmmO9NNbBSsOYD1SF7vp1lMCu9b"; // Change this
+	public static final String REST_CALLBACK_URL = "oauth://lsilberstwit"; // Change this (here and in manifest)
 
-	public RestClient(Context context) {
+	public TwitterClient(Context context) {
 		super(context, REST_API_CLASS, REST_URL, REST_CONSUMER_KEY, REST_CONSUMER_SECRET, REST_CALLBACK_URL);
 	}
 
